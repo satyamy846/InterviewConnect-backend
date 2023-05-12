b5652634e94149a026268ae9fd5253ea8a254aa8
satyamy846 <satyadav941@gmail.com>
2023-05-12 18:55:55 +0530

    feedback changed

--- models/feedback.js
+++ models/feedback.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
-
+const isEmail = require('../node_modules/validator/lib/isEmail');
 const collection = new mongoose.Schema({
     name:{
         type:String,
-
+        required:[true,"Name is required"]
     },
     email:{
         type:String,
         required:[true,"Email is required!"],
+        validate:[isEmail,"Enter valid email"],
     },
     message:{
         type:String,
